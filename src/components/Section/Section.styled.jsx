5e85352431cf44129aedde69074a20d1880ecb2b
Ruslan379 <3325528@ukr.net>
2026-02-14 16:37:02 +0200

    Next Step 14

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -7,9 +7,6 @@ export const TitleLugrasimoBold = styled.h2`
     font-weight: 700;
     font-style: normal;
 /*//todo: .title */
-    /* width: max-content; */
-    margin-left: auto;
-    margin-right: auto;
     margin-bottom: 24px;
     font-size: 48px;
     text-align: center;
