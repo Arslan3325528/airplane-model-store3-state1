f0f0602912e2d6b85d5e711d45604828d57392d3
Ruslan379 <3325528@ukr.net>
2026-02-21 00:19:55 +0200

    Step Next 45

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,18 @@
 import { Section } from '@/components/Section/Section.jsx'; 
 import planes from '@/json/planes.json'; 
-import { PlanesList } from '@/components/PlanesList/PlanesList.jsx'; 
+import { PlanesList } from '@/components/PlanesList/PlanesList.jsx';
+
+//! Збільшення/Зменьшення на 1
+import { Counter } from '@/components/Counter/Counter.jsx';
 
 export function App() {
   return (
     <>
-      <Section title="Магазин моделей літаків">
+      <Counter /> //! Збільшення/Зменьшення на 1
+
+      {/* <Section title="Магазин моделей літаків">
         <PlanesList items={planes} />
-      </Section >
+      </Section > */}
     </>
   );
 };
---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,32 @@
+// import React from "react";
+import React, { Component } from "react";
+import css from "./Counter.module.css";
+
+//! Звичайний компонент
+// export const Counter = () => {
+//     return (
+//         <div className={css.counter}>
+//             <span className={css.counterValue}>0</span>
+//             <div className={css.counterControls}>
+//                 <button className={css.buttonIncrement}>+ 1</button>
+//                 <button className={css.buttonDecrement}>- 1</button>
+//             </div>
+//         </div>
+//     )
+// };
+
+//! Компонент-клас
+// export class Counter extends React.Component {
+export class Counter extends Component {
+    render() {
+        return (
+            <div className={css.counter}>
+                <span className={css.counterValue}>0</span>
+                <div className={css.counterControls}>
+                    <button className={css.buttonIncrement}>+ 1</button>
+                    <button className={css.buttonDecrement}>- 1</button>
+                </div>
+            </div>
+        )
+    }
+}
