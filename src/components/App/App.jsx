18bf7284f2d19516f7951ad4345e0bcf0c3c65af
Ruslan379 <3325528@ukr.net>
2026-02-21 11:02:53 +0200

    Step Next 51

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import { Section } from '@/components/Section/Section.jsx'; 
 import planes from '@/json/planes.json'; 
-import helicopters from '@/json/planes.json';
-
+import helicopters from '@/json/helicopters.json';
+// import { planes, helicopters } from '@/json';
 import { PlanesList } from '@/components/PlanesList/PlanesList.jsx';
 
 //! Збільшення/Зменьшення на 1
@@ -16,7 +16,7 @@ export function App() {
 
       {/*//! Вертольоти */}
       <Section title="Магазин моделей вертольотів">
-        <PlanesList items={planes} />
+        <PlanesList items={helicopters} />
       </Section >
 
       {/*//! Літаки */}
